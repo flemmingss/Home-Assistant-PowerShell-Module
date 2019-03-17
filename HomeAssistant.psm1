5bac1ef78d5abc11385351bb2b8dfba51fe24a18
Flemming Sørvollen Skaret <flemmingss@gmail.com>
2019-03-17 01:22:19 +0100

    Create HomeAssistant.psm1

---
+++ HomeAssistant.psm1
@@ -0,0 +1,7 @@
+Write-Host "Importing functions"
+Import-Module .\Get-HomeAssistantConfig.ps1 -Verbose
+Import-Module .\Get-HomeAssistantEntity.ps1 -Verbose
+Import-Module .\Get-HomeAssistantServices.ps1 -Verbose
+Import-Module .\Invoke-HomeAssistantService.ps1 -Verbose
+Import-Module .\New-HomeAssistantSession.ps1 -Verbose
+Write-Host "Importing done"
