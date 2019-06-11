399d559342fcde41c7405c1621bba3f81b8016a0
Flemming Sørvollen Skaret <flemmingss@gmail.com>
2019-06-11 20:38:13 +0200

    Delete HomeAssistant.psm1
    Removed because of new module-file "Home-Assistant.psm1"

--- HomeAssistant.psm1
+++
@@ -1,7 +0,0 @@
-Write-Host "Importing functions"
-Import-Module .\Get-HomeAssistantConfig.ps1 -Verbose
-Import-Module .\Get-HomeAssistantEntity.ps1 -Verbose
-Import-Module .\Get-HomeAssistantServices.ps1 -Verbose
-Import-Module .\Invoke-HomeAssistantService.ps1 -Verbose
-Import-Module .\New-HomeAssistantSession.ps1 -Verbose
-Write-Host "Importing done"
